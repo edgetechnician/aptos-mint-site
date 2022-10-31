c24254e0904e36e5991ef92549ac994405a4a418
edgetechnician <49741388+edgetechnician@users.noreply.github.com>
2022-10-31 11:25:51 -0700

    Add files via upload

--- helpers/candyMachineInfo.js
+++ helpers/candyMachineInfo.js
@@ -1,11 +1,11 @@
-export const candyMachineAddress = "0xce07a74d542bc66a7deb6a68c996381cd42fc1a837b75a6c3ceb4bf156342a45";
+export const candyMachineAddress = "0x156acc8a4769de62b04a3a6894d67530942bc197ee58d0a59ef3684caa0d9660";
 export const collectionName = "Treekins"; // Case sensitive!
 export const collectionCoverUrl = "https://cloudflare-ipfs.com/ipfs/QmbNB4GzfpZJzMPw2qS5DEqbdnBJcmYDESPKA6bWyH1Dcm";
 export const mode = "test"; // "dev" or "test" or "mainnet"
 
 export let NODE_URL;
 export const CONTRACT_ADDRESS = "0x481efbf0c3cbec627b5f5674287d4ae6ee770da5949dcfe698a8520108236a33";
-export const COLLECTION_SIZE = 10
+export const COLLECTION_SIZE = 1
 let FAUCET_URL;
 if (mode == "dev") {
     NODE_URL = "https://fullnode.devnet.aptoslabs.com/v1";
